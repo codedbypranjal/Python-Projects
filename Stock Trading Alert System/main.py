bf7f9582bbfa73a0c4d62de1d024096e3ad37260
codedbypranjal <pranjal.241427@ncit.edu.np>
2026-06-18 16:55:43 +0545

    Adding multiple stocks, using loop and generalizing to avoid code repetation

--- Stock Trading Alert System/main.py
+++ Stock Trading Alert System/main.py
@@ -11,11 +11,18 @@ FROMPHONE=os.getenv('PHONE_FR')
 TOPHONE=os.getenv("PHONE_TO")
 
 
-response=requests.get(url='https://www.sharesansar.com/company/unhpl')
-soup=BeautifulSoup(response.text,'html.parser')
-price=soup.find(name="span",class_="text-comp-red comp-price padding-second")
-current_price=float(price.get_text(strip=True))
+stocks=['NABIL','NTC','CITY','KBL']
+current_price=[]
+for stock in stocks:
+    response=requests.get(url=f'https://merolagani.com/CompanyDetail.aspx?symbol={stock}')
+    soup=BeautifulSoup(response.text,'html.parser')
+    price=soup.find(name="span",id="ctl00_ContentPlaceHolder1_CompanyDetail1_lblMarketPrice")
+    f_price=float(price.get_text(strip=True))
+    dict = {stock: f_price}
+    current_price.append(dict)
 print(current_price)
+
+
 if current_price>528:
     client = Client(SID, AUTH)
     message = client.messages \
