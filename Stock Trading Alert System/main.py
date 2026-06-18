71c0c595db693cb3a4761a89021db2bd21446cfe
codedbypranjal <pranjal.241427@ncit.edu.np>
2026-06-18 16:05:00 +0545

    Removed smtplib and integrated twilio api

--- Stock Trading Alert System/main.py
+++ Stock Trading Alert System/main.py
@@ -7,9 +7,7 @@ from twilio.rest import Client
 load_dotenv()
 SID=os.getenv('ACC_SID')
 AUTH=os.getenv('AUTH_TOKEN')
-# MAIL=os.getenv('MY_MAIL')
-# PASSW=os.getenv('PASSW')
-# SMTP=os.getenv('SMTP_ADD')
+
 
 response=requests.get(url='https://www.sharesansar.com/company/unhpl')
 soup=BeautifulSoup(response.text,'html.parser')
@@ -33,14 +31,4 @@ if current_price<534:
         to='+9779741727557'
     )
 
-#
-# connection=smtplib.SMTP(SMTP)
-# connection.starttls()
-# connection.login(user=MAIL,password=PASSW)
-# if current_price>534:
-#     connection.sendmail(from_addr=MAIL,to_addrs='pranjalsapkota99@gmail.com',msg=f'Subject:Price Increased!! \n\n The price of the share increased by Rs.{current_price-534 } )')
-#     connection.close()
-# if current_price<534:
-#     connection.sendmail(from_addr=MAIL,to_addrs='pranjalsapkota99@gmail.com',msg=f'Subject:Price Decreased!! \n\n The price of the share decreased by Rs.{534-current_price } )')
-#     connection.close()
 
